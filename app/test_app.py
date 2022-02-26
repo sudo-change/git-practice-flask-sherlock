e6717474ed952dc5126189638e431aed4a9ed0fb
sudo change <63948824+sudo-change@users.noreply.github.com>
2022-02-26 11:32:50 +0530

    Update test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -11,6 +11,17 @@ def client():
 
 
 def test_api(client):
-    response = client.get("/")
+    response = client.get("/api/v1/movies/recommend")
     assert response.status_code == 200
-    assert b"Flask" in response.data
+    assert response.is_json
+    assert response.get_json()[0]["title"] != ""
+
+    response = client.get("/api/v1/movies/recommend?title=Kingpin")
+    assert response.status_code == 200
+    assert response.is_json
+    assert len(response.get_json()) >= 2
+
+    response = client.get("/api/v1/movies/recommend?title=Lost%20in%20Translation")
+    assert response.status_code == 200
+    assert response.is_json
+    assert len(response.get_json()) >= 5
