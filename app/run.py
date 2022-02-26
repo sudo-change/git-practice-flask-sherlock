9899d94a514f64f69b1613561f455b8254ca4c75
sudo change <63948824+sudo-change@users.noreply.github.com>
2022-02-26 11:32:27 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,8 +1,50 @@
+import os
+import json
+from json.decoder import JSONDecodeError
+
 from flask import Flask
+from flask import jsonify
+from flask import request
 
+from rengine import Sherlock
+
+
+# Set up app
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+APP_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def read_data(source):
+    """
+    Reads file that is expected to hold JSON encoded content.
+    In case of errors return empty data and list holding error message.
+    """
+    data = []
+    errors = []
+    try:
+        with open(source) as db:
+            content = db.read()
+        data = json.loads(content)
+    except FileNotFoundError as e:
+        errors = [f"Reading {source}, {str(e)}"]
+    except JSONDecodeError as e:
+        errors = [f"Reading {source}, {str(e)}"]
+    except Exception as e:
+        errors = [f"Reading {source}, {str(e)}"]
+
+    return data, errors
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, Flask!"
+@app.route("/api/v1/movies/recommend", methods=["GET"])
+def recommend():
+    """
+    Function loads movies from db and returns recommendations.
+    """
+    MOVIES, errors = read_data(f"{APP_DIR}/db.json")
+    if errors:
+        return jsonify({"errors": errors, "status_code": 500}), 500
+
+    sherlock = Sherlock(MOVIES, request.args)
+    recommendation = sherlock.recommend()
+
+    return jsonify(recommendation)
